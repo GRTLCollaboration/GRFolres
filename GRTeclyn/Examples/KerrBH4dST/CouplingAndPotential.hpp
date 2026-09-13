ccb89044592d0eadd39043687bfd80c032112400
Llibert Areste Salo <llibertarestesalo@gmail.com>
2026-09-13 10:04:58 +0100

    Fix comments in CouplingAndPotential files

--- GRTeclyn/Examples/KerrBH4dST/CouplingAndPotential.hpp
+++ GRTeclyn/Examples/KerrBH4dST/CouplingAndPotential.hpp
@@ -30,9 +30,9 @@ class CouplingAndPotential
         amrex::Real scalar_mass{}; // mass of the scalar field
         int coupling_type{};       // Type of coupling function to use
                                    // 0: Shift symmetric Gauss-Bonnet coupling
-        // function f(phi) = lambda * phi 1: Exponential
-        // quadratic Gauss-Bonnet coupling function f(phi)
-        // = lambda / (2 * beta) * (1 - exp(-beta * phi^2))
+        // function, f(phi) = lambda * phi.
+        // 1: Exponential quadratic Gauss-Bonnet coupling function,
+        // f(phi) = lambda / (2 * beta) * (1 - exp(-beta * phi^2)).
         amrex::Real beta{
             100.0}; // parameter for the exponential quadratic Gauss-Bonnet
                     // coupling function. Only used if coupling_type == 1
@@ -115,7 +115,7 @@ class CouplingAndPotential
     {
     }
 
-    // Set the EsGB coupling function and the scalar potential.
+    // Set the coupling functions and the scalar potential.
     // vars must provide vars.chi() and vars.phi().
     // NOLINTBEGIN(bugprone-easily-swappable-parameters)
     template <class vars_t>
